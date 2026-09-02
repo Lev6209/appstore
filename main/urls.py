f33b211974211dd9c1eb3c4e9213077391181029
Lev <lev@ishoyev.com>
2026-09-02 21:53:36 +0200

    created pages:free,top,no_category,cheap.Created FatMenu,FreeInCategory,ExpensiveInCategory,Similar functions

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('app/<int:app_id>/', views.app_detail, name='app_detail'),
     path('category/<int:category_id>/', views.category_detail, name='category'),
-    path('new/', views.new, name='new')
+    path('new/', views.new, name='new'),
+    path('top/',views.top,name='top'),
+    path('free/',views.free,name='free'),
+    path('no_category/',views.no_category,name='no_category'),
+    path('cheap/',views.cheap,name='cheap'),
+    path('free/<int:category_id>/', views.free_in_category, name='free_in_category'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -27,15 +27,41 @@ def about(request):
 
 def app_detail(request, app_id):
     app = get_object_or_404(App, id=app_id)
-    return render(request, 'main/app_detail.html', {'app': app})
+    similar_apps = App.objects.filter(price__gte=app.price - 30, price__lte=app.price + 30).exclude(id=app.id)[:3]
+    return render(request,'main/app_detail.html',{'app': app, 'similar_apps': similar_apps})
 
 
 def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     apps = App.objects.filter(category=category)
-    return render(request, 'main/category.html', {'category': category, 'apps': apps})
+    expensive = App.objects.filter(price__isnull=False, price__gt=0,category=category).order_by('-price').first()
+    return render(request, 'main/category.html', {'category': category, 'apps': apps, 'expensive': expensive})
 
 
 def new(request):
     apps = App.objects.order_by('-created_at')[:5]
     return render(request, 'main/new.html', {'apps': apps})
+
+def top(request):
+    apps = App.objects.order_by('-price').exclude(price=0)[:10]
+    return render(request, 'main/top.html', {'apps': apps})
+
+def free(request):
+    apps = App.objects.filter(price=0)
+    return render(request, 'main/free.html', {'apps': apps})
+
+def free_in_category(request, category_id):
+    apps = App.objects.filter(price=0, category_id=category_id)
+    return render(request, 'main/free.html', {'apps': apps})
+
+def no_category(request):
+    apps = App.objects.filter(category=None)
+    return render(request, 'main/no_category.html', {'apps': apps})
+
+def cheap(request):
+    apps = App.objects.order_by('price').filter(price__lt=100, price__gt=0)[:10]
+    return render(request, 'main/cheap.html', {'apps': apps})
+
+
+
+
