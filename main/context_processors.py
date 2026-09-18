45742e91ce798b51ac53d9b3cee21d87cf2de27d
Lev <lev@ishoyev.com>
2026-09-18 18:07:21 +0200

    add review counter

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,17 +1,18 @@
 from django.db.models import Count
 
 from . import apps
-from .models import App, Category
+from .models import App, Category, Review
 
 
 def store_menu(request):
     categories = list(
         Category.objects
-        .annotate(apps_count=Count('app'))
+        .annotate(apps_count=Count('app')).filter(apps_count__gt=0)
         .order_by('name')
     )
     return {
         'categories': categories,
         'apps_total': App.objects.count(),
         'categories_total': len(categories),
+        'reviews_total': Review.objects.count(),
     }
